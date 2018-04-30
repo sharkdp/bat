cc60ed656386f3435d6b73cbaa296a467e2e3d47
sharkdp <davidpeter@web.de>
2018-04-30 15:41:30 +0200

    Add binary assets

---
+++ assets/create.sh
@@ -0,0 +1,27 @@
+set -e
+
+THEME_FOLDER="$HOME/.config/bat/themes"
+SYNTAX_FOLDER="$HOME/.config/bat/syntax"
+
+if [ ! -e "$THEME_FOLDER" ]; then
+    mkdir -p "$THEME_FOLDER"
+    (
+        cd "$THEME_FOLDER"
+        git clone https://github.com/jonschlinkert/sublime-monokai-extended
+        ln -s "sublime-monokai-extended/Monokai Extended.tmTheme" Default.tmTheme
+    )
+fi
+
+if [ ! -e "$SYNTAX_FOLDER" ]; then
+    mkdir -p "$SYNTAX_FOLDER"
+    (
+        cd "$SYNTAX_FOLDER"
+        git clone https://github.com/sublimehq/Packages/
+        rm -rf Packages/Markdown
+        git clone https://github.com/jonschlinkert/sublime-markdown-extended
+    )
+fi
+
+bat init-cache
+
+cp "$HOME/.cache/bat"/* .
