556149ad3a0a85c4139cfe3dafbab45cbb4c5443
sharkdp <davidpeter@web.de>
2018-05-21 00:29:20 +0200

    Add comparison benchmark

---
+++ tests/benchmarks/comparison.sh
@@ -0,0 +1,23 @@
+if ! which hyperfine > /dev/null 2>&1; then
+    echo "'hyperfine' does not seem to be installed."
+    echo "You can get it here: https://github.com/sharkdp/hyperfine"
+    exit 1
+fi
+
+SRC="test-src/jquery-3.3.1.js"
+
+
+cmd_bat="bat --style=full --color=always --paging=never '$SRC'"
+cmd_pygmentize="pygmentize -g '$SRC'"
+cmd_highlight="highlight -O truecolor '$SRC'"
+cmd_ccat="ccat --color=always '$SRC'"
+cmd_source_highlight="source-highlight --failsafe --infer-lang -f esc -i '$SRC'"
+cmd_hicat="hicat '$SRC'"
+
+hyperfine --warmup 3 \
+    "$cmd_bat" \
+    "$cmd_pygmentize" \
+    "$cmd_highlight" \
+    "$cmd_ccat" \
+    "$cmd_source_highlight" \
+    "$cmd_hicat" \
