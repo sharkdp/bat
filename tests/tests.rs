0b1535f63b7f1484623a17a073c59f83af7d733c
sharkdp <davidpeter@web.de>
2018-08-31 22:24:36 +0200

    Split snapshot tests into individual tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -2,32 +2,34 @@ mod tester;
 
 use tester::BatTester;
 
-static STYLES: &'static [&'static str] = &[
-    "changes",
-    "grid",
-    "header",
-    "numbers",
-    "changes,grid",
-    "changes,header",
-    "changes,numbers",
-    "grid,header",
-    "grid,numbers",
-    "header,numbers",
-    "changes,grid,header",
-    "changes,grid,numbers",
-    "changes,header,numbers",
-    "grid,header,numbers",
-    "changes,grid,header,numbers",
-    "full",
-    "plain",
-];
+macro_rules! snapshot_tests {
+    ($($test_name: ident: $style: expr,)*) => {
+        $(
+            #[test]
+            fn $test_name() {
+                let bat_tester = BatTester::new();
+                bat_tester.test_snapshot($style);
+            }
+        )*
+    };
+}
 
-#[test]
-fn test_snapshots() {
-    let bat_tester = BatTester::new();
-
-    for style in STYLES {
-        println!("testing {}", style);
-        bat_tester.test_snapshot(&*style);
-    }
+snapshot_tests! {
+    changes: "changes",
+    grid: "grid",
+    header: "header",
+    numbers: "numbers",
+    changes_grid: "changes,grid",
+    changes_header: "changes,header",
+    changes_numbers: "changes,numbers",
+    grid_header: "grid,header",
+    grid_numbers: "grid,numbers",
+    header_numbers: "header,numbers",
+    changes_grid_header: "changes,grid,header",
+    changes_grid_numbers: "changes,grid,numbers",
+    changes_header_numbers: "changes,header,numbers",
+    grid_header_numbers: "grid,header,numbers",
+    changes_grid_header_numbers: "changes,grid,header,numbers",
+    full: "full",
+    plain: "plain",
 }
