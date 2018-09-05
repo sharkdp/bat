501124a437bdf5f2c6d7510920e98e1ffeca632d
Shreyansh Chouhan <chouhan.shreyansh630@gmail.com>
2018-09-05 10:19:48 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -190,6 +190,7 @@ fn run() -> Result<bool> {
             } else {
                 let mut config = app.config()?;
                 config.files = vec![InputFile::Ordinary(&"cache")];
+                
                 let assets = HighlightingAssets::new();
                 let controller = Controller::new(&config, &assets);                
                 controller.run()
