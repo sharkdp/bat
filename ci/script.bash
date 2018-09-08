e8e735c45f94607d8dba9d0eb4806f2b26608e47
rachitchokshi <rachitchokshi@gmail.com>
2018-09-08 14:23:14 -0700

    Enabled build target aarch64-unknown-linux-gnu for arm64 architecture

--- ci/script.bash
+++ ci/script.bash
@@ -6,7 +6,7 @@ set -ex
 cargo build --target "$TARGET" --verbose
 
 # We cannot run arm executables on linux
-if [[ $TARGET != arm-unknown-linux-gnueabihf ]]; then
+if [[ $TARGET != arm-unknown-linux-gnueabihf ]] && [[ $TARGET != aarch64-unknown-linux-gnu ]]; then
     cargo test --target "$TARGET" --verbose
 
     # Run 'bat' on its own source code and the README
