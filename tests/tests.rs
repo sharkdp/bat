84ac92efbbcf5ff04778f99cf2f6badf2405a840
sharkdp <davidpeter@web.de>
2018-09-12 20:32:33 +0200

    Fix test for --style=plain

--- tests/tests.rs
+++ tests/tests.rs
@@ -31,7 +31,7 @@ snapshot_tests! {
     grid_header_numbers:         "grid,header,numbers"         => [wrap: false, tabs: 8],
     changes_grid_header_numbers: "changes,grid,header,numbers" => [wrap: false, tabs: 8],
     full:                        "full"                        => [wrap: false, tabs: 8],
-    plain:                       "plain"                       => [wrap: false, tabs: 8],
+    plain:                       "plain"                       => [wrap: false, tabs: 0],
     tabs_passthrough_wrapped:    "full"                        => [wrap: true,  tabs: 0],
     tabs_4_wrapped:              "full"                        => [wrap: true,  tabs: 4],
     tabs_8_wrapped:              "full"                        => [wrap: true,  tabs: 8],
