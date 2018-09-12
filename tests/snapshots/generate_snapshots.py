b4c6e412dc01e6e2e794436ede4b953b7e26d981
sharkdp <davidpeter@web.de>
2018-09-12 20:28:42 +0200

    Make generate_snapshots script more robust

--- tests/snapshots/generate_snapshots.py
+++ tests/snapshots/generate_snapshots.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+
 import itertools
 import subprocess
 import pathlib
 import shutil
+
 
 def generate_snapshots():
     single_styles = ["changes", "grid", "header", "numbers"]
@@ -22,32 +24,40 @@ def generate_snapshots():
     generate_snapshot("tabs_8", "--tabs=8 --style=full --wrap=never")
     generate_snapshot("tabs_8_wrapped", "--tabs=8 --style=full --wrap=character")
 
+
 def generate_style_snapshot(style):
-    generate_snapshot(style.replace(",","_"), "--style={}".format(style))
+    generate_snapshot(style.replace(",", "_"), "--style={}".format(style))
+
 
 def generate_snapshot(name, arguments):
-    command = "../../target/debug/bat --decorations=always {1} sample.rs > output/{0}.snapshot.txt".format(
-        name,
-        arguments
+    command = "cargo run -- --paging=never --color=never --decorations=always "
+    command += "{args} sample.rs > output/{name}.snapshot.txt".format(
+        name=name,
+        args=arguments
     )
     print("generating snapshot for {}".format(name))
     subprocess.call(command, shell=True)
+
 
 def build_bat():
     print("building bat")
     subprocess.call("cargo build", cwd="../..", shell=True)
 
+
 def prepare_output_dir():
     shutil.rmtree("output", ignore_errors=True)
     pathlib.Path("output").mkdir()
+
 
 def modify_sample_file():
     print("modifying sample.rs to show changes")
     shutil.copyfile("sample.modified.rs", "sample.rs")
 
+
 def undo_sample_file_modification():
     print("undoing sample.rs modifications")
     subprocess.call("git checkout -- sample.rs", shell=True)
+
 
 build_bat()
 prepare_output_dir()
