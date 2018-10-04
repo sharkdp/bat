fadec4805b204620baea15114285526e5a3e8c3d
sharkdp <davidpeter@web.de>
2018-10-04 10:16:53 +0200

    Workaround for #81

--- assets/create.sh
+++ assets/create.sh
@@ -2,4 +2,12 @@
 
 ASSET_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# TODO: Remove this (and the reverse part below) when
+# https://github.com/trishume/syntect/issues/160 has been fixed
+JAVADOC_FILE="${ASSET_DIR}/syntaxes/Packages/Java/JavaDoc.sublime-syntax"
+JAVADOC_PATCH="${ASSET_DIR}/JavaDoc.sublime-syntax.patch"
+patch "$JAVADOC_FILE" "$JAVADOC_PATCH"
+
 bat cache --init --blank --source="$ASSET_DIR" --target="$ASSET_DIR"
+
+patch -R "$JAVADOC_FILE" "$JAVADOC_PATCH"
