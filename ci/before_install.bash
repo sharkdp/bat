01db6798872e511a687b5527ae9b387e127ac51d
David Peter <davidpeter@wweb.de>
2019-08-10 15:26:21 +0200

    Add libc6-dev dependency for aarch64

--- ci/before_install.bash
+++ ci/before_install.bash
@@ -33,5 +33,6 @@ if [[ $TARGET == aarch64-unknown-linux-gnu ]]; then
     sudo apt-get install -y \
         gcc-4.8-aarch64-linux-gnu \
         binutils-aarch64-linux-gnu \
-        gcc-aarch64-linux-gnu
+        gcc-aarch64-linux-gnu \
+        libc6-dev-arm64-cross
 fi
