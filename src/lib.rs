b3b46bd3a988b6cf9dcfe1fc1866e27155b1e255
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2019-10-06 09:18:08 +0700

    Fix all compile warnings in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,6 @@
 extern crate error_chain;
 
 #[macro_use]
-extern crate clap;
-
-#[macro_use]
 extern crate lazy_static;
 
 extern crate ansi_term;
@@ -21,17 +18,17 @@ extern crate shell_words;
 extern crate syntect;
 extern crate wild;
 
-mod assets;
-mod config;
-mod diff;
-mod dirs;
-mod inputfile;
-mod line_range;
-mod preprocessor;
-mod style;
-mod syntax_mapping;
-mod terminal;
-mod util;
+pub mod assets;
+pub mod config;
+pub mod diff;
+pub mod dirs;
+pub mod inputfile;
+pub mod line_range;
+pub mod preprocessor;
+pub mod style;
+pub mod syntax_mapping;
+pub mod terminal;
+pub mod util;
 
 mod errors {
     error_chain! {
@@ -41,19 +38,5 @@ mod errors {
             SyntectError(::syntect::LoadingError);
             ParseIntError(::std::num::ParseIntError);
         }
-    }
-
-    pub fn handle_error(error: &Error) {
-        match error {
-            Error(ErrorKind::Io(ref io_error), _)
-                if io_error.kind() == ::std::io::ErrorKind::BrokenPipe =>
-            {
-                ::std::process::exit(0);
-            }
-            _ => {
-                use ansi_term::Colour::Red;
-                eprintln!("{}: {}", Red.paint("[bat error]"), error);
-            }
-        };
     }
 }
