dfe5eb3e989e87db04a3af900d2c49c49308bc0b
sharkdp <davidpeter@web.de>
2020-03-21 21:45:03 +0100

    Add an even simple example

---
+++ examples/cat.rs
@@ -0,0 +1,34 @@
+/// A very simple colorized `cat` clone, using `bat` as a library.
+/// See `src/bin/bat` for the full `bat` application.
+
+use bat::{
+    config::{Config, InputFile, StyleComponent, StyleComponents},
+    Controller, HighlightingAssets,
+};
+use console::Term;
+use std::process;
+
+fn main() {
+    let files = std::env::args_os().skip(1).collect::<Vec<_>>();
+
+    if files.is_empty() {
+        eprintln!("No input files specified");
+        process::exit(1);
+    }
+
+    let config = Config {
+        term_width: Term::stdout().size().1 as usize,
+        colored_output: true,
+        true_color: true,
+        style_components: StyleComponents::new(&[
+            StyleComponent::Header,
+            StyleComponent::Grid,
+            StyleComponent::Numbers,
+        ]),
+        files: files.iter().map(|file| InputFile::Ordinary(file)).collect(),
+        ..Default::default()
+    };
+    let assets = HighlightingAssets::from_binary();
+
+    Controller::new(&config, &assets).run().expect("no errors");
+}
