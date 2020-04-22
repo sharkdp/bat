cb8ca92ce2039536a19dbad05415a492974f35af
sharkdp <davidpeter@web.de>
2020-04-22 21:16:40 +0200

    Add advanced example

---
+++ examples/advanced.rs
@@ -0,0 +1,20 @@
+/// A program that prints its own source code using the bat library
+use bat::{LineRange, PrettyPrinter, WrappingMode};
+use std::ffi::OsStr;
+
+fn main() {
+    let path_to_this_file = OsStr::new(file!());
+
+    PrettyPrinter::new()
+        .header(true)
+        .grid(true)
+        .line_numbers(true)
+        .use_italics(true)
+        // The following line will be highlighted in the output:
+        .highlight(LineRange::new(line!() as usize, line!() as usize))
+        .theme("1337")
+        .wrapping_mode(WrappingMode::Character)
+        .input_file(path_to_this_file)
+        .print()
+        .expect("no errors");
+}
