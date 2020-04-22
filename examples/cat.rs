36dde9275af3f513b77395a92a0023f7e6134fa6
sharkdp <davidpeter@web.de>
2020-04-22 20:35:05 +0200

    Simplify style_components

--- examples/cat.rs
+++ examples/cat.rs
@@ -1,16 +1,16 @@
 /// A very simple colorized `cat` clone, using `bat` as a library.
 /// See `src/bin/bat` for the full `bat` application.
-use bat::{PrettyPrinter, StyleComponent, StyleComponents};
+use bat::{PrettyPrinter, StyleComponent};
 use console::Term;
 
 fn main() {
     PrettyPrinter::new()
         .term_width(Term::stdout().size().1 as usize)
-        .style_components(StyleComponents::new(&[
+        .style_components(&[
             StyleComponent::Header,
             StyleComponent::Grid,
             StyleComponent::Numbers,
-        ]))
+        ])
         .input_files(std::env::args_os().skip(1))
         .print()
         .expect("no errors");
