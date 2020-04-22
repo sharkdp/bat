13e6b3fac7440a487f4c5952345f20ac681b571f
sharkdp <davidpeter@web.de>
2020-04-22 18:30:06 +0200

    Reduce public API

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
-/// `bat` is a library to print syntax highlighted content.
-///
-/// ```
-/// use bat::PrettyPrinter;
-///
-/// PrettyPrinter::new()
-///     .input_from_bytes(b"<span style=\"color: #ff00cc\">Hello world!</span>\n")
-///     .language("html")
-///     .run()
-///     .expect("no errors");
-/// ```
+//! `bat` is a library to print syntax highlighted content.
+//!
+//! ```
+//! use bat::PrettyPrinter;
+//!
+//! PrettyPrinter::new()
+//!     .input_from_bytes(b"<span style=\"color: #ff00cc\">Hello world!</span>\n")
+//!     .language("html")
+//!     .run()
+//!     .expect("no errors");
+//! ```
+
 pub mod assets;
 pub mod assets_metadata;
 pub mod config;
@@ -21,7 +22,7 @@ mod less;
 pub(crate) mod line_range;
 mod output;
 mod preprocessor;
-pub mod pretty_printer;
+mod pretty_printer;
 pub(crate) mod printer;
 pub(crate) mod style;
 pub(crate) mod syntax_mapping;
@@ -33,6 +34,3 @@ pub use pretty_printer::PrettyPrinter;
 pub use style::{StyleComponent, StyleComponents};
 pub use syntax_mapping::{MappingTarget, SyntaxMapping};
 pub use wrap::WrappingMode;
-
-#[cfg(feature = "paging")]
-pub use config::PagingMode;
