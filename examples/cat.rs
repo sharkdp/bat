0c9e044e4152c403691640d8c6b44fb4120554ec
sharkdp <davidpeter@web.de>
2020-04-22 21:17:59 +0200

    Update 'cat' example

--- examples/cat.rs
+++ examples/cat.rs
@@ -1,16 +1,12 @@
 /// A very simple colorized `cat` clone, using `bat` as a library.
 /// See `src/bin/bat` for the full `bat` application.
-use bat::{PrettyPrinter, StyleComponent};
-use console::Term;
+use bat::PrettyPrinter;
 
 fn main() {
     PrettyPrinter::new()
-        .term_width(Term::stdout().size().1 as usize)
-        .style_components(&[
-            StyleComponent::Header,
-            StyleComponent::Grid,
-            StyleComponent::LineNumbers,
-        ])
+        .header(true)
+        .grid(true)
+        .line_numbers(true)
         .input_files(std::env::args_os().skip(1))
         .print()
         .expect("no errors");
