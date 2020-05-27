2ab647476132c10419bc0ff79e0aeaf0c7be0c79
sharkdp <davidpeter@web.de>
2020-05-27 08:47:07 +0200

    Add unit test to make sure we are not missing any themes

---
+++ tests/assets.rs
@@ -0,0 +1,35 @@
+use bat::assets::HighlightingAssets;
+
+/// This test ensures that we are not accidentally removing themes due to submodule updates.
+#[test]
+fn all_themes_are_present() {
+    let assets = HighlightingAssets::from_binary();
+
+    let mut themes: Vec<_> = assets.themes().collect();
+    themes.sort();
+
+    assert_eq!(
+        themes,
+        vec![
+            "1337",
+            "DarkNeon",
+            "Dracula",
+            "GitHub",
+            "Monokai Extended",
+            "Monokai Extended Bright",
+            "Monokai Extended Light",
+            "Monokai Extended Origin",
+            "Nord",
+            "OneHalfDark",
+            "OneHalfLight",
+            "Solarized (dark)",
+            "Solarized (light)",
+            "Sublime Snazzy",
+            "TwoDark",
+            "ansi-dark",
+            "ansi-light",
+            "base16",
+            "zenburn"
+        ]
+    );
+}
