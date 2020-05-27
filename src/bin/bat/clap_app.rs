c638de4f5a395a3e32a2505b1fd1c4cd8177e27f
Ethan P <eth-p+git@hidden.email>
2020-05-27 16:38:43 -0700

    Add support for NO_COLOR env var (#1021)

--- src/bin/bat/clap_app.rs
+++ src/bin/bat/clap_app.rs
@@ -1,8 +1,9 @@
 use clap::{crate_name, crate_version, App as ClapApp, AppSettings, Arg, ArgGroup, SubCommand};
+use std::env;
 use std::path::Path;
 
 pub fn build_app(interactive_output: bool) -> ClapApp<'static, 'static> {
-    let clap_color_setting = if interactive_output {
+    let clap_color_setting = if interactive_output && env::var_os("NO_COLOR").is_none() {
         AppSettings::ColoredHelp
     } else {
         AppSettings::ColorNever
