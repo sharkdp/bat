fc121f0c8702a23951596850f7bec2bb2de5b45b
Caio Alonso <caio@caioalonso.com>
2020-06-26 21:42:05 -0300

    Defaults *.fs to F#

--- src/syntax_mapping.rs
+++ src/syntax_mapping.rs
@@ -23,6 +23,7 @@ impl<'a> SyntaxMapping<'a> {
     pub fn builtin() -> SyntaxMapping<'a> {
         let mut mapping = Self::empty();
         mapping.insert("*.h", MappingTarget::MapTo("C++")).unwrap();
+        mapping.insert("*.fs", MappingTarget::MapTo("F#")).unwrap();
         mapping
             .insert("build", MappingTarget::MapToUnknown)
             .unwrap();
