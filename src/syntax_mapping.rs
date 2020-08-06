cab5ddf6ae91c8a84bf34385831a30963713755c
Kienyew <31496021+Kienyew@users.noreply.github.com>
2020-08-06 15:20:33 +0800

    Fix Typo

--- src/syntax_mapping.rs
+++ src/syntax_mapping.rs
@@ -94,10 +94,10 @@ impl<'a> SyntaxMapping<'a> {
 
     pub(crate) fn get_syntax_for(&self, path: impl AsRef<Path>) -> Option<MappingTarget<'a>> {
         let candidate = Candidate::new(path.as_ref());
-        let canddidate_filename = path.as_ref().file_name().map(Candidate::new);
+        let candidate_filename = path.as_ref().file_name().map(Candidate::new);
         for (ref glob, ref syntax) in self.mappings.iter().rev() {
             if glob.is_match_candidate(&candidate)
-                || canddidate_filename
+                || candidate_filename
                     .as_ref()
                     .map_or(false, |filename| glob.is_match_candidate(filename))
             {
