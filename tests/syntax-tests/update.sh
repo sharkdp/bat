51d25e5859171e060eb6edd6a1de758b4bc125ce
Ethan P <eth-p+git@hidden.email>
2020-10-04 00:31:28 -0700

    Fix syntax highlighting test update.sh to work on Mac OS

--- tests/syntax-tests/update.sh
+++ tests/syntax-tests/update.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python create_highlighted_versions.py -O highlighted
+python="python3"
+if ! command -v python3 &>/dev/null; then python="python"; fi
+"$python" create_highlighted_versions.py -O highlighted
