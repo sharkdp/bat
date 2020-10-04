b50bd587954e8467cf6141b8c86ca416e74fb26c
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2020-10-04 11:37:37 +0200

    Replace #!/bin/bash with #!/usr/bin/env bash to improve compatibility with systems where /bin/bash is not available

--- tests/benchmarks/run-benchmarks.sh
+++ tests/benchmarks/run-benchmarks.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if ! which hyperfine > /dev/null 2>&1; then
     echo "'hyperfine' does not seem to be installed."
--- tests/syntax-tests/update.sh
+++ tests/syntax-tests/update.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 python="python3"
 if ! command -v python3 &>/dev/null; then python="python"; fi
