60406c7c2d581cb9110e02e7388e405afc831557
sharkdp <davidpeter@web.de>
2021-01-01 23:17:56 +0100

    Exhaustive list of relevant environment variables

--- src/bin/bat/main.rs
+++ src/bin/bat/main.rs
@@ -237,11 +237,17 @@ fn run() -> Result<bool> {
             .info(EnvironmentVariables::list(&[
                 "SHELL",
                 "PAGER",
-                "BAT_PAGER",
+                "BAT_CACHE_PATH",
                 "BAT_CONFIG_PATH",
+                "BAT_OPTS",
+                "BAT_PAGER",
                 "BAT_STYLE",
-                "BAT_THEME",
                 "BAT_TABS",
+                "BAT_THEME",
+                "XDG_CONFIG_HOME",
+                "XDG_CACHE_HOME",
+                "COLORTERM",
+                "NO_COLOR",
             ]))
             .info(FileContent::new("Config file", config_file()))
             .info(CompileTimeInformation::default())
