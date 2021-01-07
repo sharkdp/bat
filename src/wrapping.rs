ac96cbf8309c7081c26b08e9ac5a7654be77f8e5
mark chaitin <markchaitin@gmail.com>
2021-01-06 20:17:00 -0500

    fix bug where long lines were truncated in plain mode without wrap=never not being set

--- src/wrapping.rs
+++ src/wrapping.rs
@@ -1,11 +1,11 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum WrappingMode {
     Character,
-    NoWrapping,
+    NoWrapping(bool), // explicitly opted in or not
 }
 
 impl Default for WrappingMode {
     fn default() -> Self {
-        WrappingMode::NoWrapping
+        WrappingMode::NoWrapping(false)
     }
 }
