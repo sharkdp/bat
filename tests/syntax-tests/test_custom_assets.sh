61edfdd23f7fd70640da33549b7d6c626bdb6668
Martin Nordholts <enselic@gmail.com>
2021-09-06 19:48:51 +0200

    Use BAT_CONFIG_DIR and BAT_CACHE_PATH

--- tests/syntax-tests/test_custom_assets.sh
+++ tests/syntax-tests/test_custom_assets.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
+### ENVIRONMENT
+
+BAT_CONFIG_DIR=$(mktemp -d)
+export BAT_CONFIG_DIR
+
+BAT_CACHE_PATH=$(mktemp -d)
+export BAT_CACHE_PATH
+
+echo "
+BAT_CONFIG_DIR = ${BAT_CONFIG_DIR}
+BAT_CACHE_PATH = ${BAT_CACHE_PATH}
+"
+
 ### HELPER VARS
-
-custom_syntaxes_dir="$(bat --config-dir)/syntaxes"
-
-installed_custom_syntax="${custom_syntaxes_dir}/BatTestCustomAssets.sublime-syntax"
 
 custom_syntax_args=(
     "--language=BatTestCustomAssets"
@@ -24,7 +33,7 @@ echo_step() {
 }
 
 fail_test() {
-    echo "FAIL: $1"
+    echo -e "FAIL: $1"
     exit 1
 }
 
@@ -32,12 +41,13 @@ fail_test() {
 
 echo_step "TEST: Make sure 'BatTestCustomAssets' is not part of integrated syntaxes"
 bat -f "${custom_syntax_args[@]}" &&
-    fail_test "EXPECTED: 'unknown syntax' error ACTUAL: no error occured
-               HINT: try manually doing 'bat cache --clear' before running tests"
+    fail_test "EXPECTED: 'unknown syntax' error ACTUAL: no error occured"
 
 echo_step "PREPARE: Install custom syntax 'BatTestCustomAssets'"
+custom_syntaxes_dir="$(bat --config-dir)/syntaxes"
 mkdir -p "${custom_syntaxes_dir}"
-cp -v "tests/syntax-tests/BatTestCustomAssets.sublime-syntax" "${installed_custom_syntax}"
+cp -v "tests/syntax-tests/BatTestCustomAssets.sublime-syntax" \
+    "${custom_syntaxes_dir}/BatTestCustomAssets.sublime-syntax"
 
 echo_step "PREPARE: Build custom assets to enable 'BatTestCustomAssets' syntax"
 bat cache --build
@@ -48,12 +58,17 @@ bat -f "${custom_syntax_args[@]}" ||
 
 echo_step "TEST: The 'Rust' syntax is still available"
 bat -f "${integrated_syntax_args[@]}" ||
-    fail_test "EXPECTED: syntax highlighting to still work for integrated assets ACTUAL: there was an error"
+    fail_test "EXPECTED: syntax highlighting still works with integrated assets ACTUAL: there was an error"
 
 echo_step "TEST: 'BatTestCustomAssets' is an unknown syntax with --no-custom-assets"
 bat -f --no-custom-assets "${custom_syntax_args[@]}" &&
-    fail_test "EXPECTED: 'unknown syntax' error because of --no-custom-assets, but no error occured"
+    fail_test "EXPECTED: 'unknown syntax' error because of --no-custom-assets ACTUAL: no error occured"
 
-echo_step "CLEANING: To avoid unwanted side effects of running tests"
+echo_step "TEST: 'bat cache --clear' removes all files"
 bat cache --clear
-rm -rfv "${installed_custom_syntax}"
+remaining_files=$(ls -A "${BAT_CACHE_PATH}")
+[ -z "${remaining_files}" ] ||
+    fail_test "EXPECTED: no files remain ACTUAL: some files remain:\n${remaining_files}"
+
+echo_step "CLEAN"
+rm -rv "${BAT_CONFIG_DIR}" "${BAT_CACHE_PATH}"
