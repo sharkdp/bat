85d8cc8d713dc4fc6e60fc6296452817f591ed90
Dan Davison <dandavison7@gmail.com>
2022-02-06 23:32:52 -0500

    Document public functions

--- src/assets.rs
+++ src/assets.rs
@@ -90,6 +90,7 @@ impl HighlightingAssets {
         self.fallback_theme = Some(theme);
     }
 
+    /// Return the collection of syntect syntax definitions.
     pub fn get_syntax_set(&self) -> Result<&SyntaxSet> {
         self.syntax_set_cell
             .get_or_try_init(|| self.serialized_syntax_set.deserialize())
@@ -186,6 +187,7 @@ impl HighlightingAssets {
         }
     }
 
+    /// Look up a syntect theme by name.
     pub fn get_theme(&self, theme: &str) -> &Theme {
         match self.get_theme_set().get(theme) {
             Some(theme) => theme,
