fccdada5c012fbccd4022ff90249ba72a2f0a94d
Dan Davison <dandavison7@gmail.com>
2022-02-06 22:59:04 -0500

    Revert "Use new type pattern instead of enum with one variant"
    This reverts commit 3d019910e9a2b0696e9ff84be684921ed82689d3.

--- src/assets/theme_set.rs
+++ src/assets/theme_set.rs
@@ -3,16 +3,22 @@ use super::lazy_theme_set::LazyThemeSet;
 /// A collection of syntect themes, similar to [`syntect::highlighting::ThemeSet`].
 /// The themes are deserialized on-demand only.
 #[derive(Debug)]
-pub struct ThemeSet(pub LazyThemeSet);
+pub enum ThemeSet {
+    LazyThemeSet(LazyThemeSet),
+}
 
 impl ThemeSet {
     /// Look up a theme by name
     pub fn get(&self, name: &str) -> Option<&syntect::highlighting::Theme> {
-        self.0.get(name)
+        match self {
+            ThemeSet::LazyThemeSet(lazy_theme_set) => lazy_theme_set.get(name),
+        }
     }
 
     /// An iterator over all theme names
     pub fn theme_names(&self) -> impl Iterator<Item = &str> {
-        self.0.themes()
+        match self {
+            ThemeSet::LazyThemeSet(lazy_theme_set) => lazy_theme_set.themes(),
+        }
     }
 }
