d0eab78cc3cd02deb7678654c06d8f216fbc47d6
Martin Nordholts <enselic@gmail.com>
2022-07-03 14:44:51 +0200

    src/pager.rs nitpick: arg0 -> s
    I forgot to comment on this name so I figured I'd just push a commit to
    take care of it.

--- src/pager.rs
+++ src/pager.rs
@@ -49,7 +49,7 @@ impl PagerKind {
 
         // Check if the current running binary is set to be our pager.
         let is_current_bin_pager = current_bin
-            .map(|arg0| Path::new(&arg0).file_stem() == pager_bin)
+            .map(|s| Path::new(&s).file_stem() == pager_bin)
             .unwrap_or(false);
 
         match pager_bin.map(|s| s.to_string_lossy()).as_deref() {
