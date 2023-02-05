f5dbfb3a5702dc2ba25be3da05c19a64bd94ae59
einfachIrgendwer0815 <85333734+einfachIrgendwer0815@users.noreply.github.com>
2023-02-05 10:42:13 +0100

    Update comment

--- src/config.rs
+++ src/config.rs
@@ -39,7 +39,7 @@ pub struct Config<'a> {
     /// Whether or not to show/replace non-printable characters like space, tab and newline.
     pub show_nonprintable: bool,
 
-    /// Whether or not to use caret notation to show non-printable characters.
+    /// The configured notation for non-printable characters
     pub nonprinting_notation: String,
 
     /// The character width of the terminal
