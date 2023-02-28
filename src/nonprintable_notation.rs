80d8ed20795f3d123cec7fad1595c61c5fac84e3
einfachIrgendwer0815 <85333734+einfachIrgendwer0815@users.noreply.github.com>
2023-02-28 14:10:40 +0100

    Automatically derive Default for NonprintableNotation

--- src/nonprintable_notation.rs
+++ src/nonprintable_notation.rs
@@ -1,11 +1,7 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
 pub enum NonprintableNotation {
     Caret,
-    Unicode,
-}
 
-impl Default for NonprintableNotation {
-    fn default() -> Self {
-        NonprintableNotation::Unicode
-    }
+    #[default]
+    Unicode,
 }
