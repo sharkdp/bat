ddcc6cf53bf38da456cdd45c1e58dda03f8157ac
Martin Nordholts <enselic@gmail.com>
2023-03-14 21:13:46 +0100

    Mark NonprintableNotation #[non_exhaustive]

--- src/nonprintable_notation.rs
+++ src/nonprintable_notation.rs
@@ -1,6 +1,7 @@
 /// How to print non-printable characters with
 /// [crate::config::Config::show_nonprintable]
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+#[non_exhaustive]
 pub enum NonprintableNotation {
     /// Use caret notation (^G, ^J, ^@, ..)
     Caret,
