b000db8f32af8f8d608184ac5ec3413b46d0bf5b
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2023-10-27 14:18:36 +0800

    Remove old completion generation code

--- build.rs
+++ build.rs
@@ -1,6 +1,3 @@
-// TODO: Re-enable generation of shell completion files (below) when clap 3 is out.
-// For more details, see https://github.com/sharkdp/bat/issues/372
-
 // For bat-as-a-library, no build script is required. The build script is for
 // the manpage and completions, which are only relevant to the bat application.
 #[cfg(not(feature = "application"))]
@@ -79,30 +76,3 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
-
-// #[macro_use]
-// extern crate clap;
-
-// use clap::Shell;
-// use std::fs;
-
-// include!("src/clap_app.rs");
-
-// const BIN_NAME: &str = "bat";
-
-// fn main() {
-//     let outdir = std::env::var_os("SHELL_COMPLETIONS_DIR").or(std::env::var_os("OUT_DIR"));
-
-//     let outdir = match outdir {
-//         None => return,
-//         Some(outdir) => outdir,
-//     };
-
-//     fs::create_dir_all(&outdir).unwrap();
-
-//     let mut app = build_app(true);
-//     app.gen_completions(BIN_NAME, Shell::Bash, &outdir);
-//     app.gen_completions(BIN_NAME, Shell::Fish, &outdir);
-//     app.gen_completions(BIN_NAME, Shell::Zsh, &outdir);
-//     app.gen_completions(BIN_NAME, Shell::PowerShell, &outdir);
-// }
