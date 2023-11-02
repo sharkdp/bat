d0b9fba6146713a0c5a58d7c336565c14c42fcbd
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2023-11-02 23:29:42 +0800

    Rerun build script only when necessary

--- build/main.rs
+++ build/main.rs
@@ -3,6 +3,10 @@ mod application;
 mod util;
 
 fn main() -> anyhow::Result<()> {
+    // only watch manually-designated files
+    // see: https://doc.rust-lang.org/cargo/reference/build-scripts.html#rerun-if-changed
+    println!("cargo:rerun-if-changed=build/");
+
     #[cfg(feature = "application")]
     application::gen_man_and_comp()?;
 
