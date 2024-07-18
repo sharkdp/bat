30b0143ccf93edac1310fca522a89550d5566676
Tau Gärtli <git@tau.garden>
2024-07-18 15:44:33 +0200

    Make default_theme pub

--- src/theme.rs
+++ src/theme.rs
@@ -10,7 +10,9 @@ pub fn theme(options: ThemeOptions) -> String {
     theme_from_detector(options, &TerminalColorSchemeDetector)
 }
 
-pub(crate) const fn default_theme(color_scheme: ColorScheme) -> &'static str {
+/// The default theme, suitable for the given color scheme.
+/// Use [`theme`], if you want to automatically detect the color scheme from the terminal.
+pub const fn default_theme(color_scheme: ColorScheme) -> &'static str {
     match color_scheme {
         ColorScheme::Dark => "Monokai Extended",
         ColorScheme::Light => "Monokai Extended Light",
