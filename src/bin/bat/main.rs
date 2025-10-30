1852dea96a9c943db4f36fdd34b61d3d662c52d9
abhinavcool42 <abhinavcool42@gmail.com>
2025-10-30 18:42:35 +0530

    Fix --list-themes pager init so less stays interactive

--- src/bin/bat/main.rs
+++ src/bin/bat/main.rs
@@ -205,6 +205,7 @@ pub fn list_themes(
     style.insert(StyleComponent::Plain);
     config.language = Some("Rust");
     config.style_components = StyleComponents(style);
+    config.paging_mode = PagingMode::Always;
 
     let mut output_type =
         OutputType::from_mode(config.paging_mode, config.wrapping_mode, config.pager)?;
